ff2c087a1e1d9bae1bafb0bf90ee1ca2a06a2471
Marcello Feroce <ferocemarcello@gmail.com>
2024-06-15 17:31:23 +0200

    modified baby.hs

--- app/baby.hs
+++ app/baby.hs
@@ -5,3 +5,10 @@ doubleSmallNumber x = if x > 100
                         else x*2
 doubleSmallNumber' x = doubleSmallNumber x + 1
 conanO'Brien = "It's a-me, Conan O'Brien!"
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+length' xs = sum [1 | _ <- xs]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+xxs = [[1,3,5,2,3,1,2,4,5],[1,2,3,4,5,6,7,8,9],[1,2,4,2,1,6,3,1,3,2,3,6]]
+triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
+rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
+rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
