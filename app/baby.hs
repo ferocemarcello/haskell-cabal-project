8d21434dc33b05da12a7c267ddedaf2712d5601e
Marcello Feroce <ferocemarcello@gmail.com>
2024-06-17 15:36:28 +0200

    working on types

--- app/baby.hs
+++ app/baby.hs
@@ -7,8 +7,17 @@ doubleSmallNumber' x = doubleSmallNumber x + 1
 conanO'Brien = "It's a-me, Conan O'Brien!"
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 length' xs = sum [1 | _ <- xs]
-removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 xxs = [[1,3,5,2,3,1,2,4,5],[1,2,3,4,5,6,7,8,9],[1,2,4,2,1,6,3,1,3,2,3,6]]
 triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
 rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
 rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
+removeNonUppercase :: String -> String
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+factorial :: Integer -> Integer  
+factorial n = product [1..n]
+circumference :: Float -> Float  
+circumference r = 2 * pi * r
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r 
