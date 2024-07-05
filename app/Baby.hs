9448c2192450b26fe2aca42066f212955f40eb86
Marcello Feroce <ferocemarcello@gmail.com>
2024-07-05 13:42:59 +0200

    updating conf

---
+++ app/Baby.hs
@@ -0,0 +1,79 @@
+module Baby where
+import Text.Read
+
+doubleMe x = x + x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100  
+                        then x  
+                        else x*2
+doubleSmallNumber' x = doubleSmallNumber x + 1
+conanO'Brien = "It's a-me, Conan O'Brien!"
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+length' xs = sum [1 | _ <- xs]
+xxs = [[1,3,5,2,3,1,2,4,5],[1,2,3,4,5,6,7,8,9],[1,2,4,2,1,6,3,1,3,2,3,6]]
+triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
+rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
+rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
+removeNonUppercase :: String -> String
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+factorial :: Integer -> Integer  
+factorial n = product [1..n]
+circumference :: Float -> Float  
+circumference r = 2 * pi * r
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"  
+lucky x = "Sorry, you're out of luck, pal!"
+sayMe :: (Integral a) => a -> String  
+sayMe 1 = "One!"  
+sayMe 2 = "Two!"  
+sayMe 3 = "Three!"  
+sayMe 4 = "Four!"  
+sayMe 5 = "Five!"  
+sayMe x = "Not between 1 and 5"
+factorial' :: (Integral a) => a -> a  
+factorial' 0 = 1  
+factorial' n = n * factorial' (n - 1)
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)  
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+first :: (a, b, c) -> a  
+first (x, _, _) = x  
+second :: (a, b, c) -> b  
+second (_, y, _) = y  
+third :: (a, b, c) -> c  
+third (_, _, z) = z
+head' :: [a] -> a  
+head' [] = error "Can't call head on an empty list, dummy!"  
+head' (x:_) = x
+tell :: (Show a) => [a] -> String  
+tell [] = "The list is empty"  
+tell [x] = "The list has one element: " ++ show x  
+tell [x,y] = "The list has two elements: " ++ show x ++ " and " ++ show y  
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+length'' :: (Num b) => [a] -> b  
+length'' [] = 0  
+length'' (_:xs) = 1 + length' xs
+capital :: String -> String  
+capital "" = "Empty string, whoops!"  
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x] 
+densityTell :: (RealFloat a) => a -> a -> String  
+densityTell mass volume  
+    | mass / volume < 1.2 = "Wow! You're going for a ride in the sky!"  
+    | mass / volume <= 1000.0 = "Have fun swimming, but watch out for sharks!"  
+    | otherwise   = "If it's sink or swim, you're going to sink."
+max' :: (Ord a) => a -> a -> a  
+max' a b | a > b = a | otherwise = b
+myCompare :: (Ord a) => a -> a -> Ordering  
+a `myCompare` b  
+    | a > b     = GT  
+    | a == b    = EQ  
+    | otherwise = LT
+densityTell' :: String -> String  
+densityTell' input  
+    | Just density <- readMaybe input, density < 1.2 = "Wow! You're going for a ride in the sky!"  
+    | Just density <- readMaybe input, density <= 1000.0 = "Have fun swimming, but watch out for sharks!"  
+    | Nothing <- readMaybe input :: (RealFloat a => Maybe a) = "You know I need a density, right?"  
+    | otherwise   = "If it's sink or swim, you're going to sink."
